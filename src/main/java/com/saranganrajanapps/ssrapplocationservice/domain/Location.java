e9eb7f078abb3f8862b8ed3b6f1df67d174421d6
Sarangapani Varadharajan <sarangapani.singapore@gmail.com>
2024-01-10 03:34:30 +0800

    test commit

--- src/main/java/com/saranganrajanapps/ssrapplocationservice/domain/Location.java
+++ src/main/java/com/saranganrajanapps/ssrapplocationservice/domain/Location.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonSerialize
+@Getter
 public class Location {
     @JsonProperty("id")
     String id;
